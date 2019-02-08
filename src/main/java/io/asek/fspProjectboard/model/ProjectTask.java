6c5028c777928c7179e3f94869264452905ba76b
Andy Sekulovski <andy.sek94@gmail.com>
2019-02-08 17:41:28 +1100

    project task entity

---
+++ src/main/java/io/asek/fspProjectboard/model/ProjectTask.java
@@ -0,0 +1,40 @@
+package io.asek.fspProjectboard.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "project_tasks")
+public class ProjectTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String summary;
+    private String description;
+    private String status;
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
