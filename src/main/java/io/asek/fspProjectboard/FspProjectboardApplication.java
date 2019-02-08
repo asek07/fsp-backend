9fed82f4cbee8fa1507e1c2ef86a7fde253aca77
Andy Sekulovski <andy.sek94@gmail.com>
2019-02-08 17:03:09 +1100

    imported pom succesfully

---
+++ src/main/java/io/asek/fspProjectboard/FspProjectboardApplication.java
@@ -0,0 +1,14 @@
+package io.asek.fspProjectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FspProjectboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FspProjectboardApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/io/asek/fspProjectboard/FspProjectboardApplicationTests.java
@@ -0,0 +1,17 @@
+package io.asek.fspProjectboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FspProjectboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
